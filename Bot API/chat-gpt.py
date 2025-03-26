9916265c365d50cf999832ccfa8d73a5beb76860
ViveksuriyanS <viveksuriyancs93@gmail.com>
2025-03-26 15:37:05 +0530

    Changes

--- Bot API/chat-gpt.py
+++ Bot API/chat-gpt.py
@@ -7,7 +7,7 @@ import httpx
 #openai.api_client.transport = client
 
 os.environ['REQUESTS_CA_BUNDLE'] = '/Users/vs032332/Downloads/CSRootCA.pem'
-openai.api_key = "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"
+#openai.api_key = "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"
 
 
 def chat_with_gpt(prompt):
